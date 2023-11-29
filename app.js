fb74bd058321c09328741c2c26fcb73224ebbaaf
fabiorngit <114315121+fabiorngit@users.noreply.github.com>
2023-11-28 22:03:46 -0300

    Add style

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let body = document.querySelector('body')
+
+function dark(){
+
+    body.classList.add('dark')
+
+
+// if(body.style.classList==='dark'){
+// body.style.classList.remove('light')
+// }else{
+//     body.style.classList==='dark' 
+// }
+
+
+}
+
+function light(){
+    body.classList.remove('dark');
+    body.classList.add('light');
+
+
+
+}
